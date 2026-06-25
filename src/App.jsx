6fe530db20afbf901255baf027a084fd7b19a0a9
JOHN CARLO R. DULUTAN <202310776@fit.edu.ph>
2026-06-25 12:49:34 +0800

    refactor(routes): restructure routing for ProtectedRoute and AuthenticatedLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import AuthenticatedLayout from './components/AuthenticatedLayout';
 import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
 import Landing from './pages/Landing';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
@@ -15,16 +17,14 @@ export default function App() {
         <Route index element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
-        <Route
-          path="dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AuthenticatedLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="tasks" element={<Tasks />} />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
